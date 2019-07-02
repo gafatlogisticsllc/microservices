e776465516542d083b487d2c0ae3dab3132c22df
GAFATLOGISTICSLLC <52306534+GAFATLOGISTICSLLC@users.noreply.github.com>
2019-07-02 14:19:58 -0500

    Third commit

---
+++ netflix-eureka-naming-server/zipkin-distributed-tracing-service/src/main/java/com/gafat/microservices/zipkindistributedtracingservice/ZipkinDistributedTracingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.gafat.microservices.zipkindistributedtracingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZipkinDistributedTracingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZipkinDistributedTracingServiceApplication.class, args);
+	}
+
+}
---
+++ netflix-eureka-naming-server/zipkin-distributed-tracing-service/src/test/java/com/gafat/microservices/zipkindistributedtracingservice/ZipkinDistributedTracingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gafat.microservices.zipkindistributedtracingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZipkinDistributedTracingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
